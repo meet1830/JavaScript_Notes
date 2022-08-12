0843db08412cf38f06a6c6a27067dd17c2cc5d0f
meet <mehtameet1234@gmail.com>
2022-08-12 16:50:13 +0530

    updated notes

--- 07_arrays_and_objects.js
+++ 07_arrays_and_objects.js
@@ -1,27 +1,25 @@
 const marks = [24, 34, 53, 53, 53, 82, 84, 26, 28];
-const fruits = ['orange', 'apple', 'pineapple'];
-const mixed = ['str', 54, [53, 654]];
+const fruits = ["orange", "apple", "pineapple"];
+const mixed = ["str", 54, [53, 654]];
 
-let arr = new Array(12, 41, 62, 'orange');
+let arr = new Array(12, 41, 62, "orange");
 
 console.log(arr);
 console.log(mixed);
-console.log(fruits[1]); 
-
+console.log(fruits[1]);
 
 // two kinds -> methods and properties
 
-console.log(arr.length); 
+console.log(arr.length);
 // length is a prop of array, hence no need of () after properties
 
-console.log(Array.isArray(arr)); 
+console.log(Array.isArray(arr));
 // returns if it is array or not true or false
 // method hence ()
 
-arr[0] = 'meet';
+arr[0] = "meet";
 // hence can change array values
 
- 
 let value = marks.indexOf(53);
 console.log(value);
 
@@ -35,17 +33,17 @@ marks.unshift(64);
 
 console.log(marks);
 
-marks.pop(); 
+marks.pop();
 // removes the last element
 
 console.log(marks);
 
-marks.shift(); 
+marks.shift();
 // removes first element
 
 console.log(marks);
 
-marks.splice(2, 4); 
+marks.splice(2, 4);
 // starts at 2nd index, and removes 4 elements
 
 console.log(marks);
@@ -68,31 +66,30 @@ marks3 = marks3.concat(marks2);
 console.log(marks3);
 
 let myObj = {
-    name: 'meet',
-    'last name': 'mehta',
-    channel: 'youtube',
-    isActive: true,
-    marks: [29, 52, 58, 57]
-}
+  name: "meet",
+  "last name": "mehta",
+  channel: "youtube",
+  isActive: true,
+  marks: [29, 52, 58, 57],
+};
 
 console.log(myObj.name);
 
 console.log(myObj.isActive);
 
-console.log(myObj['last name']);
+console.log(myObj["last name"]);
 
-console.log(myObj['name']);
+console.log(myObj["name"]);
 
 // when need to store elements use array but when need to store key values then use objects
 
-
 let pavan = {
-    firstName : 'Pavan',
-    lastName : 'Kumar',
-    sayHi : function () {
-        console.log("Hi");
-    }
-}
+  firstName: "Pavan",
+  lastName: "Kumar",
+  sayHi: function () {
+    console.log("Hi");
+  },
+};
 console.log(pavan.firstName);
 pavan.sayHi();
 
@@ -107,18 +104,23 @@ console.log(console.log("hi"));
 
 // here for nested console hi is printed but since console.log is a function and does not return a value, which means undefined, the outer function prints that
 
+
 // THIS keyword in javascript
 pavan = {
-    firstName : "pavan",
-    lastName : "kumar",
-    displayName : function () {
-        console.log("my name is " + this.firstName + " " + this.lastName);
-    },
-    hobbies : ['swimming', 'cycling', 'jogging'],
-    displayIntro : function () {
-        console.log(`My name is ${this.firstName} ${this.lastName} and my hobbies are ${this.hobbies.join(', ')}.`);
-    }
-}
+  firstName: "pavan",
+  lastName: "kumar",
+  displayName: function () {
+    console.log("my name is " + this.firstName + " " + this.lastName);
+  },
+  hobbies: ["swimming", "cycling", "jogging"],
+  displayIntro: function () {
+    console.log(
+      `My name is ${this.firstName} ${
+        this.lastName
+      } and my hobbies are ${this.hobbies.join(", ")}.`
+    );
+  },
+};
 pavan.displayName();
 // if write just firstName and lastName then will give error of Reference type
 // this means owner of that method
@@ -129,17 +131,18 @@ pavan.displayIntro();
 // join is an array method which converts array to a string on the basis of the separator that is mentioned here comma and a space
 
 
+
 // Javascript object constructors
-function Student (firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.displayName = function () {
-        console.log(this.firstName + " " + this.lastName);
-    }
+function Student(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.displayName = function () {
+    console.log(this.firstName + " " + this.lastName);
+  };
 }
 
-let meet = new Student ('Meet', 'Mehta');
-let rahul = new Student ('Rahul', 'Jaiswal');
+let meet = new Student("Meet", "Mehta");
+let rahul = new Student("Rahul", "Jaiswal");
 // here all the objects created have type student
 
 console.log(meet);
@@ -149,6 +152,7 @@ console.log(rahul);
 
 meet.displayName();
 // Meet Mehta
+
 
 
 // array methods continued
@@ -171,13 +175,12 @@ arr.filter(function(element) {
 let data = [943, 5243, 529, 52, 492, 52925, 52, 529];
 
 // get all odd numbers
-let filteredArr = data.filter(function(element) {
-    return element % 2 == 1;
+let filteredArr = data.filter(function (element) {
+  return element % 2 == 1;
 });
 
 console.log(filteredArr);
 // (5) [943, 5243, 529, 52925, 529]
-
 
 /*
 3. Map
@@ -195,18 +198,17 @@ arr.map(function(element) {
 
 data = [943, 5243, 529, 52, 492, 52925, 52, 529];
 
-let updatedArr = data.map(function(element) {
-    // return element + 5;
-    
-    // return element; --> will return the same element values
-    
-    // return; --> will return an array of undefined same size as input arr, for each iteration we dont return anything hence undefined array
-    // or leave the loop empty with no code inside and print will result in same output as above
+let updatedArr = data.map(function (element) {
+  // return element + 5;
 
-    if (element % 2 === 0) 
-        return element;
-    // output undefined where not return and ans where possible
-})
+  // return element; --> will return the same element values
+
+  // return; --> will return an array of undefined same size as input arr, for each iteration we dont return anything hence undefined array
+  // or leave the loop empty with no code inside and print will result in same output as above
+
+  if (element % 2 === 0) return element;
+  // output undefined where not return and ans where possible
+});
 
 console.log(updatedArr);
 // (8) [948, 5248, 534, 57, 497, 52930, 57, 534]
@@ -215,12 +217,13 @@ console.log(updatedArr);
 console.log(data.includes(5243));
 // true
 
-console.log(data.includes('943'));
+console.log(data.includes("943"));
 // false -> js does not convert here
 
 
+
 // objects also has methods -> object.keys(), object.values(), object.entries()
-let obj1 = {a : 5, b : 10};
+let obj1 = { a: 5, b: 10 };
 console.log(Object.keys(obj1));
 // (2) ['a', 'b']
 // returns an array of keys of the object
@@ -230,16 +233,17 @@ console.log(Object.values(obj1));
 // returns an array of values of the object
 
 
+
 // Es6 SPREAD operator (...) (populate array elements / object entries)
 
 arr = [1, 5, 3, 2, 4];
 // newArr = arr, 6, 7 -> [1, 5, 3, 2, 4, 6, 7];
-// newarr should contain all the elements of arr and also 6 and 7 
+// newarr should contain all the elements of arr and also 6 and 7
 
 // conventional method without using spread operator
-let newArr = []
+let newArr = [];
 for (let i = 0; i < arr.length; i++) {
-    newArr[i] = arr[i];
+  newArr[i] = arr[i];
 }
 newArr.push(6, 7);
 // newArr[arr.length] = 6;
@@ -247,8 +251,8 @@ newArr.push(6, 7);
 console.log(newArr);
 
 // conventional method 2 -> using map
-let newArr2 = arr.map(function(element) {
-    return element;
+let newArr2 = arr.map(function (element) {
+  return element;
 });
 newArr2.push(6, 7);
 console.log(newArr2);
@@ -258,7 +262,7 @@ let arr1 = [1, 5, 3, 2, 4];
 let arr2 = [6, 7];
 let newArr3 = arr1;
 for (let i = 0; i < arr2.length; i++) {
-    newArr[arr.length + i] = arr2[i];
+  newArr[arr.length + i] = arr2[i];
 }
 console.log(newArr);
 
@@ -272,51 +276,49 @@ console.log(newArr4);
 let newArr5 = [...arr1, ...arr2];
 console.log(newArr5);
 
-
 // using spread operator for merging two objects into a new object
 
 // conventional method using for-in loop
-obj1 = { a: 5, b: 10 }
-let obj2 = { c: 6, d: 12 }
-let obj3 = {}
+obj1 = { a: 5, b: 10 };
+let obj2 = { c: 6, d: 12 };
+let obj3 = {};
 for (let key in obj1) {
-    obj3[key] = obj1[key]
+  obj3[key] = obj1[key];
 }
 for (let key in obj2) {
-    obj3[key] = obj2[key]
+  obj3[key] = obj2[key];
 }
 // if same key exists in obj1 and obj2 then will update the value in obj3 or create new key
-obj3['first name'] = 'meet';
+obj3["first name"] = "meet";
 // this will add a new property first name in the object
-console.log(obj3)
+console.log(obj3);
 // {a: 5, b: 10, c: 6, d: 12}
 
 // using spread operator
 obj3 = {
-    ...obj1,
-    ...obj2
-}
-console.log(obj3)
+  ...obj1,
+  ...obj2,
+};
+console.log(obj3);
 // {a: 5, b: 10, c: 6, d: 12}
 
 obj3 = {
-    a: 5,
-    b: 10,
-    c: 15,
-    a: 20
-}
+  a: 5,
+  b: 10,
+  c: 15,
+  a: 20,
+};
 console.log(obj3);
 // {a: 20, b: 10, c: 15}
 // values can be same for different keys in object but keys are unique for the object
 // if the key is repeated in the object then will not give error, the latest assigned value will be taken as the key value
 
-
 // Cloning an object and changing only required fields -> now if want one object to be same as new object but with one or more values changed then
 let obj5 = {
-    a: 5,
-    b: 10,
-    c: 100
-}
+  a: 5,
+  b: 10,
+  c: 100,
+};
 let obj6 = obj5;
 obj6.a = 10;
 console.log(obj5, obj6);
@@ -324,57 +326,61 @@ console.log(obj5, obj6);
 // above is wrong because this updates obj5 as well
 
 let obj7 = {
-    a: 5,
-    b: 10,
-    c: 100
-}
+  a: 5,
+  b: 10,
+  c: 100,
+};
 let obj8 = {
-    ...obj7,
-    a: 10
-}
+  ...obj7,
+  a: 10,
+};
 console.log(obj7, obj8);
 // {a: 5, b: 10, c: 100} {a: 10, b: 10, c: 100}
 
 obj8 = {
-    a: 10, 
-    ...obj7
-}
+  a: 10,
+  ...obj7,
+};
 console.log(obj8);
 // {a: 5, b: 10, c: 100}
 // value will not get updated
 
 
+
 // OBJECT PROPERTY ASSIGNMENT
 // condition -> key name of object = variable name holding keys value
 
-let firstName = 'meet'
-let lastName = 'mehta'
+let firstName = "meet";
+let lastName = "mehta";
 let obj9 = {
-    firstName: firstName,
-    lastName: lastName
-}
-console.log(obj9)
+  firstName: firstName,
+  lastName: lastName,
+};
+console.log(obj9);
 // {firstName: 'meet', lastName: 'mehta'}
 
 // in es6 javascript
 obj9 = {
-    firstName, 
-    lastName
-}
-console.log(obj9)
+  firstName,
+  lastName,
+};
+console.log(obj9);
 // {firstName: 'meet', lastName: 'mehta'}
-// this is called object property assignment 
+// this is called object property assignment
 // if following condition then can write less code in this syntax
 // it will read as firstName is the key and its value is stored in variable named firstName
 
-// see destructing in js file now for es6 syntax
+
+
+// see destructing in js file now for es6 syntax in arrays
+
 
 
 // OBJECT.ENTRIES -> gives array of arrays (inner array of size 2 -> key and value)
 let obj10 = {
-    a: 34,
-    b: 85
-}
+  a: 34,
+  b: 85,
+};
 console.log(Object.entries(obj10));
 /* (2) [Array(2), Array(2)]
 0: (2) ['a', 34]
@@ -384,36 +390,42 @@ length: 2
 */
 
 
+
 // Array.find method
 // find method takes in function as an argument, return a condition to find the element -> if value exists then returns back value, else returns undefined
 arr = [3, 5, 4, 1, 7];
 console.log(arr.find((element) => element === 3));
 // 3
-console.log(arr.find((element) => { return element === 6 }));
+console.log(
+  arr.find((element) => {
+    return element === 6;
+  })
+);
 // undefined
 // not returning in first due to arrow function syntax
 // similar method is findIndex
 // find also exists for objects
 
 
+
 // Object.assign -> cloning (shallow)
 obj1 = {
-    a: 5,
-    b: 10
-}
+  a: 5,
+  b: 10,
+};
 let obj11 = obj1;
 obj11.b = 15;
 console.log(obj11, obj1);
 // {a: 5, b: 15} {a: 5, b: 15}
 // here obj1 also gets updated with 5 and 15
-let x = 5
+let x = 5;
 let y = x;
 y = 15;
-console.log(x, y)
+console.log(x, y);
 // 5 15
 // here x and y are primitive values and hence behave like this. objects are non primitive and hence cannot be copied like this
 
-// in js when object is created, it is created in stack and is referenced to heap memory. when another object is copied to that object, it is again pointing to same first object or referenced to same first object. 
+// in js when object is created, it is created in stack and is referenced to heap memory. when another object is copied to that object, it is again pointing to same first object or referenced to same first object.
 // hence here no new object is created when assigning one object to a new one and it points to the same object
 // hence any changes in any of both objects will reflect in both the objects
 // cloning means creating new object same as the object mentioned and the new object will point to the new object created
@@ -425,26 +437,25 @@ console.log(obj1, obj11);
 // {a: 5, b: 15} {a: 5, b: 30}
 
 // creating a shallow copy -> method 2 -> using Object.assign()
-let obj12 = { a: 3, b: 67 }
+let obj12 = { a: 3, b: 67 };
 let obj13 = Object.assign({}, obj12);
 // syntax -> default value of newly created object, and extra parameters that are needed to be added into the new object created
-console.log(obj13)
+console.log(obj13);
 // {a: 3, b: 67}
-
 
 // deep copy
 let obj14 = {
-    a: 56,
-    b: 45,
-    c: {
-        d: 34,
-        e: 25
-    }
-}
+  a: 56,
+  b: 45,
+  c: {
+    d: 34,
+    e: 25,
+  },
+};
 let obj15 = Object.assign({}, obj14);
-obj15.a = 156
-obj15.c.e = 125
-console.log(obj14, obj15)
+obj15.a = 156;
+obj15.c.e = 125;
+console.log(obj14, obj15);
 /*
 {a: 56, b: 45, c: {…}}
 a: 56
@@ -459,10 +470,10 @@ c: {d: 34, e: 125}
 // here we can see that using assign the outer values were not referenced and were recreated properly, but the nested object was referenced from the parent object
 
 // in order to solve this we use json methods
-let obj16 = JSON.parse(JSON.stringify(obj14))
-obj16.a = 156
-obj16.c.e = 125
-console.log(obj14, obj16)
+let obj16 = JSON.parse(JSON.stringify(obj14));
+obj16.a = 156;
+obj16.c.e = 125;
+console.log(obj14, obj16);
 /*
 {a: 56, b: 45, c: {…}}
 a: 56
@@ -475,3 +486,176 @@ b: 45
 c: {d: 34, e: 125}
 */
 // first the object is converted to a string, and using parse method a new object is created
+
+
+
+// EXERCISE 1
+let students = [
+  {
+    name: "Rama",
+    batch: 9,
+    likesIcecream: true,
+  },
+  {
+    name: "Irfan",
+    batch: 8,
+    likesIcecream: false,
+  },
+  {
+    name: "Swapneel",
+    batch: 7,
+    likesIcecream: false,
+  },
+  {
+    name: "Sampath",
+    batch: 8,
+    likesIcecream: true,
+  },
+  {
+    name: "Vishwanath",
+    batch: 9,
+    likesIcecream: true,
+  },
+];
+
+// Q - Return array of only those students who like ice cream
+
+// Solution 1
+let studentsWhoLikeIceCream1 = [];
+students.forEach((student) => {
+  if (student.likesIcecream) {
+    studentsWhoLikeIceCream1.push(student);
+  }
+});
+console.log(studentsWhoLikeIceCream1);
+/*
+(3) [{…}, {…}, {…}]
+    0: {name: 'Rama', batch: 9, likesIcecream: true}
+    1: {name: 'Sampath', batch: 8, likesIcecream: true}
+    2: {name: 'Vishwanath', batch: 9, likesIcecream: true}
+    length: 3
+    [[Prototype]]: Array(0)
+*/
+
+// solution 1 using arrow functions
+let studentsWhoLikeIceCream2 = [];
+students.forEach((student) =>
+  student.likesIcecream ? studentsWhoLikeIceCream2.push(student) : ""
+);
+console.log(studentsWhoLikeIceCream2);
+// same output
+
+// Solution 2
+let studentsWhoLikeIceCream3 = students.filter((student) => {
+  return student.likesIcecream;
+});
+console.log(studentsWhoLikeIceCream3);
+// same output
+
+// solution 2 using arrow functions
+let studentsWhoLikeIceCream4 = students.filter(
+  (student) => student.likesIcecream
+);
+console.log(studentsWhoLikeIceCream4);
+// same output
+
+
+
+// EXERCISE 2
+let studentsInfo = [
+  {
+    name: "Vaishnavi",
+    batch: 8,
+    marks: {
+      science: 80,
+      social: 70,
+      hindi: 80,
+      maths: 100,
+      english: 90,
+    },
+  },
+  {
+    name: "Abhishek",
+    batch: 10,
+    marks: {
+      science: 75,
+      social: 75,
+      hindi: 62,
+      maths: 81,
+      english: 52,
+    },
+  },
+  {
+    name: "Pappu",
+    batch: 10,
+    marks: {
+      science: 70,
+      social: 72,
+      hindi: 75,
+      maths: 95,
+      english: 82,
+    },
+  },
+  {
+    name: "Ram",
+    batch: 7,
+    marks: {
+      science: 35,
+      social: 24,
+      hindi: 22,
+      maths: 95,
+      english: 67,
+    },
+  },
+  {
+    name: "Shyam",
+    batch: 9,
+    marks: {
+      science: 45,
+      social: 87,
+      hindi: 65,
+      maths: 78,
+      english: 99,
+    },
+  },
+];
+
+/* Return array of students with following details:
+    - name
+    - batch
+    - average marks secured
+*/
+
+let studentsDetails = studentsInfo.map(student => {
+    let marksArray = Object.values(student.marks);
+    let totalMarks = 0;
+    marksArray.forEach(marks => {
+        totalMarks += marks;
+    })
+    let averageMarks = totalMarks / marksArray.length;
+
+    return {
+        name : student.name,
+        batch : student.batch,
+        averageMarks
+    }
+})
+
+console.log(studentsDetails);
+/*
+(5) [{…}, {…}, {…}, {…}, {…}]
+    0: {name: 'Vaishnavi', batch: 8, averageMarks: 84}
+    1: {name: 'Abhishek', batch: 10, averageMarks: 69}
+    2: {name: 'Pappu', batch: 10, averageMarks: 78.8}
+    3: {name: 'Ram', batch: 7, averageMarks: 48.6}
+    4: {name: 'Shyam', batch: 9, averageMarks: 74.8}
+    length: 5
+    [[Prototype]]: Array(0)
+*/
+
+
+/* Q - Return array of only those students who have an avergae marks > 75 with following details:
+	- name
+	- batch
+	- average marks secured
+*/
--- 37_callback_functions.js
+++ 37_callback_functions.js
@@ -8,7 +8,6 @@ const students = [
   { name: "meet2", subject: "machine learning" },
 ];
 
-
 function getStudents() {
   setTimeout(function () {
     let str = "";
@@ -22,13 +21,12 @@ function getStudents() {
   //settimeout function manages function in the background, after a given time interval.
 }
 
-
 function enrollStudents(student) {
-    setTimeout(function () {
-        students.push(student);
-        console.log('Student has been enrolled.')
-    }, 3000);
-    //settimeout is a callback function. it manages function in the background, after a given time interval.
+  setTimeout(function () {
+    students.push(student);
+    console.log("Student has been enrolled.");
+  }, 3000);
+  //settimeout is a callback function. it manages function in the background, after a given time interval.
 }
 
 let newStudent = { name: "sunny", subject: "python" };
@@ -41,26 +39,23 @@ let newStudent = { name: "sunny", subject: "python" };
 // this means that if both functions are called together then they will execute together and the one with less timeout will finish first and then the function with greater timeout.
 // callback functions can be synchronous or async. foreach function is sync func.
 
-
 function enrollStudents(student, callback) {
   setTimeout(function () {
     students.push(student);
     console.log("Student has been enrolled.");
     callback(); //can give any name
-  }, 1000);  
-}  
+  }, 1000);
+}
 /*
 callback functions in javascript
 Student has been enrolled.
 Students have been fetched.
 */
 
-
 newStudent = { name: "sunny", subject: "python" };
 enrollStudents(newStudent, getStudents);
 //getstudents is our callback func here.
 // here callback function = getstudents and parent function = enroll students. here parent will first start to execute after its timeout. then at the line of callback function call, will start to execute it. but since callback itself has timeout, will keep executing parent function and callback function both together.
-
 
 function enrollStudents(student, callback) {
   setTimeout(function () {
@@ -70,8 +65,8 @@ function enrollStudents(student, callback) {
     setTimeout(() => {
       console.log("2 sec time");
     }, 2000);
-  }, 1000); 
-} 
+  }, 1000);
+}
 /*
 callback functions in javascript
 Student has been enrolled.
@@ -79,4 +74,196 @@ Student has been enrolled.
 Students have been fetched.
 */
 
-// here the callback func has time 3 sec where as the 2 sec time has 2 sec. hence the 2 sec time if is printed first and then the students are fetched.  
+// here the callback func has time 3 sec where as the 2 sec time has 2 sec. hence the 2 sec time if is printed first and then the students are fetched.
+
+/* Higher order functions have one of the following props
+-> passes another function as argument (callbacks)
+-> returns/nest another function (closures)
+
+callbacks -> when a function definition is passed as an argument to another function
+*/
+
+function abc(callback) {
+  console.log("abc");
+  callback();
+}
+
+abc(function () {
+  console.log("def");
+});
+
+// using callbacks, we can define a function outside and then just call it in parent function. why to pass it as an argument
+// callbacks can handle async code -> if the call back function is defined outside and called without passing it as an arguement, then it will not be able to process its output. there are three ways to do this -> callbacks, promises, async await
+
+// using setTimeout to mock api requests
+let firstFivePrimeNumbers = [];
+function fetchRecords() {
+  setTimeout(function () {
+    firstFivePrimeNumbers = [2, 3, 5, 7, 11];
+  });
+}
+console.log(firstFivePrimeNumbers);
+// the output will be empty array
+// and this will directly be executed bc sync code executes first
+
+let firstFourPrimeNumbers = [];
+function fetchRecords(callback) {
+  setTimeout(function () {
+    firstFourPrimeNumbers = [2, 3, 5, 7];
+    callback();
+  }, 5000);
+}
+fetchRecords(function () {
+  console.log(firstFourPrimeNumbers);
+});
+// output after two seconds
+// (4) [2, 3, 5, 7]
+
+setTimeout(() => {
+  console.log(1);
+}, 5000);
+
+setTimeout(() => {
+  console.log(2);
+}, 3000);
+
+setTimeout(() => {
+  console.log(3);
+}, 4000);
+// 2 3 1
+// here in 5 seconds all three functions are completed. because they run independently
+
+// how to handle the timeout functions such that they log out 1, 2 and then 3?
+
+setTimeout(() => {
+  console.log(1);
+
+  setTimeout(() => {
+    console.log(2);
+
+    setTimeout(() => {
+      console.log(3);
+    }, 3000);
+  }, 4000);
+}, 5000);
+// this can be a solution
+// after 5 seconds 1 logs, then after 4 seconds 2 logs out and after 3 seconds 3 logs out. hence it takes 12 sec to log out 1 2 3
+
+// hence it is async code but we want it to execute in a pattern that is why callbacks are used
+
+/*
+Order placed
+After 2s -> Chef received the order and started preparing
+After 10s -> pizza sauce added
+After 5s -> first layer of cheese added
+After 12s -> topping added
+After 5s -> second layer of cheese added
+After 15s -> pizza baked!
+After 8s -> oregano added and packed
+After 2s -> pizza received at counter
+The order is ready and handed over to the zomato guy!
+*/
+// the reception gets updated with these steps when they are completed.
+
+console.log("Order placed");
+
+setTimeout(() => {
+  console.log("Chef received the order and started preparing");
+}, 2000);
+
+setTimeout(() => {
+  console.log("pizza sauce added");
+}, 10000);
+
+setTimeout(() => {
+  console.log("first layer of cheese added");
+}, 5000);
+
+setTimeout(() => {
+  console.log("toppings added");
+}, 12000);
+
+setTimeout(() => {
+  console.log("second layer of cheese added");
+}, 5000);
+
+setTimeout(() => {
+  console.log("pizza baked!");
+}, 15000);
+
+setTimeout(() => {
+  console.log("oregano added and packed");
+}, 8000);
+
+setTimeout(() => {
+  console.log("pizza received at counter");
+}, 2000);
+
+console.log("The order is ready and handed over to the zomato guy!");
+/*
+Order placed
+The order is ready and handed over to the zomato guy!
+Chef received the order and started preparing
+pizza received at counter
+first layer of cheese added
+second layer of cheese added
+oregano added and packed
+pizza sauce added
+toppings added
+pizza baked!
+*/
+// since this is async code it does not run in order
+
+// use callbacks to determine the order of execution
+function executeOrder(callback) {
+  console.log("Order placed");
+
+  setTimeout(() => {
+    console.log("Chef received the order and started preparing");
+
+    setTimeout(() => {
+      console.log("pizza sauce added");
+
+      setTimeout(() => {
+        console.log("first layer of cheese added");
+
+        setTimeout(() => {
+          console.log("toppings added");
+
+          setTimeout(() => {
+            console.log("second layer of cheese added");
+
+            setTimeout(() => {
+              console.log("pizza baked!");
+
+              setTimeout(() => {
+                console.log("oregano added and packed");
+
+                setTimeout(() => {
+                  console.log("pizza received at counter");
+
+                  callback();
+                }, 2000);
+              }, 8000);
+            }, 15000);
+          }, 5000);
+        }, 12000);
+      }, 5000);
+    }, 10000);
+  }, 2000);
+}
+/*
+Order placed
+Chef received the order and started preparing
+pizza sauce added
+first layer of cheese added
+toppings added
+second layer of cheese added
+pizza baked!
+oregano added and packed
+pizza received at counter
+The order is ready and handed over to the zomato guy!
+*/
+
+// this is called callback hell and it should be avoided
+// instead use async await and promises
